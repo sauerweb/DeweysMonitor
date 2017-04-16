26b9607753e1498d9c201b23ac6ed6d428f26ac1
Brett Sauerwein <brett.sauerwein@gmail.com>
2017-04-16 15:29:33 -0500

    fix previous commit

--- source/tweet.py
+++ source/tweet.py
@@ -2,7 +2,7 @@ import private_credentials
 import tweepy
 
 
-def Send(text):
+def send(text):
    auth = tweepy.OAuthHandler(private_credentials.consumer_key, private_credentials.consumer_secret)
    auth.set_access_token(private_credentials.access_token, private_credentials.access_token_secret)
    api = tweepy.API(auth)
