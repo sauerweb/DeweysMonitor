e2acb08902a9fed7ca7a076ddda6a232338b2744
Brett Sauerwein <brett.sauerwein@gmail.com>
2017-04-19 09:09:47 -0500

    limited tweets t 140 characters

--- source/tweet.py
+++ source/tweet.py
@@ -7,4 +7,5 @@ def send(text):
    auth.set_access_token(private_credentials.access_token, private_credentials.access_token_secret)
    api = tweepy.API(auth)
 
+   text = text[:140]
    api.update_status(status=text)
